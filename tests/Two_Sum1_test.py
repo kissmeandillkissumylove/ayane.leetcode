d0813ae0ee0df065dce06e77ca7b33fb5f9ec126
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-03-02 19:22:58 +0700

    add tests for solution 1

---
+++ tests/Two_Sum1_test.py
@@ -0,0 +1,12 @@
+from code.code.Two_Sum1 import Solution
+from unittest import TestCase, main
+
+
+class TestTwo_Sum1(TestCase):
+	def test_twoSum(self):
+		self.assertEqual(Solution.twoSum([2,7,11,15], 9), [0, 1])
+		self.assertEqual(Solution.twoSum([3,2,4], 6), [1,2])
+		self.assertEqual(Solution.twoSum([3,3], 6), [0, 1])
+
+if __name__ == "__main__":
+	main()
